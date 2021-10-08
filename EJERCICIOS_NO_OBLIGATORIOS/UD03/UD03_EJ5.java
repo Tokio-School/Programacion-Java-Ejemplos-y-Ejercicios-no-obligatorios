47288949e734a4e647ac7edff7d3dccb6a742cc2
Tic Tokio <68330929+TokioSchool@users.noreply.github.com>
2021-10-08 13:22:13 +0200

    Rename Ejercicio1_1.java to UD03_EJ5.java

--- EJERCICIOS_NO_OBLIGATORIOS/UD03/UD03_EJ5.java
+++ EJERCICIOS_NO_OBLIGATORIOS/UD03/UD03_EJ5.java
@@ -6,14 +6,14 @@ public class Ejercicio1_1 {
 
 	public static void main(String[] args) {
 		
-		/*Escribe un programa que escriba por pantalla toda la secuencia entre dos n˙meros introducidos por el usuario (un valor mÌnimo y
-		uno m·ximo). Realiza las comprobaciones previas sobre los datos introducidos*/
+		/*Escribe un programa que escriba por pantalla toda la secuencia entre dos n√∫meros introducidos por el usuario (un valor m√≠nimo y
+		uno m√°ximo). Realiza las comprobaciones previas sobre los datos introducidos*/
 		
 		Scanner keyboard = new Scanner(System.in);
 
-        System.out.print("Introduzca el valor mÌnimo: ");
+        System.out.print("Introduzca el valor m√≠nimo: ");
         String number1 = keyboard.nextLine();
-        System.out.print("Introduzca el valor m·ximo: ");
+        System.out.print("Introduzca el valor m√°ximo: ");
         String number2 = keyboard.nextLine();
 
         int minValue = Integer.parseInt(number1);
