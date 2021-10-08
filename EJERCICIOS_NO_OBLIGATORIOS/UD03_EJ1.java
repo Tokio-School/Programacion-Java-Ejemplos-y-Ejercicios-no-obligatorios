230301f93a8a1282e4d7de3b36d884d076c44441
Tic Tokio <68330929+TokioSchool@users.noreply.github.com>
2021-10-08 12:17:22 +0200

    Rename Ejercicio1.java to UD03_EJ1.java

--- EJERCICIOS_NO_OBLIGATORIOS/UD03_EJ1.java
+++ EJERCICIOS_NO_OBLIGATORIOS/UD03_EJ1.java
@@ -6,18 +6,18 @@ public class Ejercicio1 {
 
 	public static void main(String[] args) {
 		
-		//Escribe un programa que indique, para dos números dados, cuál es mayor
+		//Escribe un programa que indique, para dos nÃºmeros dados, cuÃ¡l es mayor
 		
 		Scanner keyboard = new Scanner(System.in);
 
-        System.out.print("Introduzca el primer número: ");
+        System.out.print("Introduzca el primer nÃºmero: ");
         String number1 = keyboard.nextLine();
-        System.out.print("Introduzca el segundo número: ");
+        System.out.print("Introduzca el segundo nÃºmero: ");
         String number2 = keyboard.nextLine();
 
-        // Comprobar si los valores introducidos son correctos (numéricos)
+        // Comprobar si los valores introducidos son correctos (numÃ©ricos)
         if ((!number1.matches("[0-9]*")) || (!number2.matches("[0-9]*"))) {
-            System.out.println("Alguno de los números introducidos no es correcto");
+            System.out.println("Alguno de los nÃºmeros introducidos no es correcto");
             // Sale del programa
             System.exit(0);
         }
@@ -26,9 +26,9 @@ public class Ejercicio1 {
         int value2 = Integer.parseInt(number2);
 
         if (value1 > value2) {
-            System.out.println("El primer número es mayor");
+            System.out.println("El primer nÃºmero es mayor");
         } else if (value2 > value1){
-            System.out.println("El segundo número es mayor");
+            System.out.println("El segundo nÃºmero es mayor");
         } else {
             System.out.println("Son iguales");
         }
