99c8ca32d28ba259af2322c700bb01584d83eeec
Tic Tokio <68330929+TokioSchool@users.noreply.github.com>
2021-10-08 12:52:18 +0200

    Add files via upload

---
+++ EJERCICIOS_NO_OBLIGATORIOS/UD06/TokioCalendarV2/Constants.java
@@ -0,0 +1,7 @@
+package com.tokioschool.tokiocalendar.util;
+
+public class Constants {
+    public static final String DEFAULT_TASK_DESCRIPTION = "<Sin descripción>";
+    public static final String DEFAULT_EVENT_DESCRIPTION = "<Sin descripción>";
+    public static final int REMINDER_TIME_DEFAULT = 15;
+}
