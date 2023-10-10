ec9651ada3fe93dc3008c4559224ffeab1f0a2f1
Victor Vergel <victorvergel@verinsis.com>
2023-10-10 20:01:02 +0200

    Mientras estamos en clase

--- ClasesVictorVergel/Clase09_IntroduccionPOO/src/ejem01/clases/Aplicacion.java
+++ ClasesVictorVergel/Clase09_IntroduccionPOO/src/ejem01/clases/Aplicacion.java
@@ -4,7 +4,17 @@ public class Aplicacion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Persona juan=new Persona();
+		
+		Persona pepe=new Persona("Pepe");
+		
+		Persona maria=new Persona("Maria", 18, false);
+		
+		Persona miguelAngel=new Persona("Miguel Angel", 19, false);
+		
+		
+		
+		
 	}
 
 }
---
+++ ClasesVictorVergel/Clase09_IntroduccionPOO/src/ejem01/clases/Persona.java
@@ -0,0 +1,30 @@
+package ejem01.clases;
+
+public class Persona {
+	String nombre;
+	int edad;
+	boolean casado;
+	
+	
+	public Persona() {
+	}
+
+
+	public Persona(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public Persona(String nombre, int edad, boolean casado) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.casado = casado;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
