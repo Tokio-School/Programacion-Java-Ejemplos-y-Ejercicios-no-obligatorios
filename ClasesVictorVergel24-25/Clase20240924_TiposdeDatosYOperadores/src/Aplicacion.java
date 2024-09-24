68dc81f345ef40a4739080e27339bf587fcd7382
Víctor Vergel <victorvergel@gregoriofer.com>
2024-09-24 20:04:09 +0200

    Mitad de la clase

--- ClasesVictorVergel24-25/Clase20240924_TiposdeDatosYOperadores/src/Aplicacion.java
+++ ClasesVictorVergel24-25/Clase20240924_TiposdeDatosYOperadores/src/Aplicacion.java
@@ -3,7 +3,22 @@ public class Aplicacion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo");
+		byte edad=18;
+		byte edad2=127;
+		
+		int numero=123456;
+		
+		System.out.println(edad);
+		
+		float altura=1.75f;
+		
+		char letraA1='a';
+		char letraA2=97;
+		System.out.println(letraA1+"-"+letraA2);
+		
+		boolean esMayor=false;
+		System.out.println(esMayor);
+		
 	}
 
 }
