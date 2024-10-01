ee94885e53d947d4490a9f1ba8b5c5c7b1eea113
Víctor Vergel <victorvergel@gregoriofer.com>
2024-10-01 19:42:30 +0200

    Clase Scanner

--- ClasesVictorVergel24-25/Clase20241001_TiposDatosYOperadoresII/src/Aplicacion.java
+++ ClasesVictorVergel24-25/Clase20241001_TiposDatosYOperadoresII/src/Aplicacion.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 
 public class Aplicacion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner teclado=new Scanner(System.in);
+		System.out.println("Introduce tu nombre:");
+		String dato=teclado.nextLine();
+		System.out.println("hola mundo de "+dato);
+		
+		
+		
 	}
 
 }
