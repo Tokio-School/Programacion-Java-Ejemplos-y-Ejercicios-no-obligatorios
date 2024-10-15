ba396020c384458161b35c1d1742c89266cf62b3
Víctor Vergel <victorvergel@verinsis.com>
2024-10-15 20:36:06 +0200

    Finalizació nde la clase de Sentencias de control II y Powerpoint de la presentación.

---
+++ ClasesVictorVergel24-25/Clase20241015_SentenciasControlII/src/Piramides.java
@@ -0,0 +1,28 @@
+
+public class Piramides {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5 - i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k <= i; k++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5 - i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k <= i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
